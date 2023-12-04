56a28c24020fca0eb913ca0d7fe0595afbe56353
Martyna Dudzińska <150166810+martynadudzinska@users.noreply.github.com>
2023-12-04 19:14:39 +0100

    main commit

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,8 +1,15 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Heloo");
+       JFrame okno = new JFrame();
+       okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       okno.setResizable(false);
+       okno.setTitle("Across the Universe");
+       //okno.setSize(1280,1024);
+       okno.setLocationRelativeTo(null);
+       okno.setVisible(true);
 
         }
     }
